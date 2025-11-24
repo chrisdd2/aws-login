5b2522b428aff4d87d23db052d2dcebe38d3f0bf
CD <chris.damianidis2@gmail.com>
2025-11-24 16:07:57 +0200

    use new constructs in main

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"log/slog"
@@ -17,71 +16,7 @@ import (
 	"github.com/chrisdd2/aws-login/auth"
 	"github.com/chrisdd2/aws-login/aws"
 	"github.com/chrisdd2/aws-login/storage"
-	"github.com/chrisdd2/aws-login/webui"
-	"github.com/chrisdd2/aws-login/webui/templates"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
-
-func NewMemoryStorage(filename string) (storage.Storage, func(), error) {
-	store := storage.NewMemoryStorage()
-
-	f, err := os.Open(filename)
-	if err != nil {
-		log.Println(err)
-	} else {
-		if err := store.LoadFromReader(f); err != nil {
-			return nil, nil, fmt.Errorf("store.LoadFromReader [%w]", err)
-		}
-	}
-	defer f.Close()
-	flushFunc := func(s *storage.MemoryStorage) {
-		log.Printf("saving storage to [%s]\n", filename)
-		f, err := os.Create(filename)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		if err := s.SaveToWriter(f); err != nil {
-			f.Close()
-			log.Fatalln(err)
-		}
-		f.Close()
-		f, err = os.Open(filename)
-		if err := s.LoadFromReader(f); err != nil {
-			log.Fatalln(err)
-		}
-		defer f.Close()
-	}
-	store.SetFlush(flushFunc)
-	exitSignal := make(chan os.Signal, 10)
-	signal.Notify(exitSignal, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-exitSignal
-		store.Flush()
-		os.Exit(1)
-	}()
-	return store, store.Flush, nil
-}
-
-type storageCtx struct {
-	store    storage.Storage
-	saveFunc func()
-}
-
-func prepareStorage(cfg *AppConfig) (storageCtx, error) {
-	switch cfg.StorageType {
-	case StorageTypeSql:
-		log.Printf("Using SQL storage backend: %s", cfg.DatabaseUrl)
-		store, err := storage.NewSQLStorage(cfg.DatabaseUrl)
-		return storageCtx{store: store, saveFunc: func() {}}, err
-	case StorageTypeMemory:
-		log.Printf("Using in-memory storage backend (MemoryStorage)")
-		store, saveStorage, err := NewMemoryStorage(cfg.StorageFile)
-		return storageCtx{store: store, saveFunc: saveStorage}, err
-	default:
-		return storageCtx{}, fmt.Errorf("unknown storage type %s", cfg.StorageType)
-	}
-}
 
 func must(err error) {
 	if err != nil {
@@ -94,6 +29,17 @@ func must2[T any](a T, err error) T {
 	}
 	return a
 }
+func must3[T any, Y any](a T, b Y, err error) (T, Y) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return a, b
+}
+func assert(cond bool, msg string) {
+	if !cond {
+		log.Fatalln(msg)
+	}
+}
 
 func main() {
 	appCfg := AppConfig{}
@@ -101,14 +47,37 @@ func main() {
 	must(appCfg.LoadDefaults())
 	must(appCfg.LoadFromEnv())
 
+	var logger *slog.Logger
+	if appCfg.DevelopmentMode {
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
+	} else {
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{}))
+	}
+	slog.SetDefault(logger)
+
 	appCfg.debugPrint()
 
-	storageCtx := must2(prepareStorage(&appCfg))
-	defer storageCtx.saveFunc()
+	ctx := context.Background()
 
+	// storage setup
+	assert(appCfg.StorageType == "memory", "only memory storage support atm")
+	storage := must2(storage.NewJsonBackend(appCfg.StorageFile))
+
+	exitSignal := make(chan os.Signal, 10)
+	signal.Notify(exitSignal, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-exitSignal
+		err := storage.Close()
+		if err != nil {
+			logger.Error("error saving storage [%s]", err)
+		}
+		os.Exit(1)
+	}()
+
+	// AWS setup
 	// allow different aws config for the aws user used for permissions in the other accounts
 	stsClient := must2(withEnvContext(appCfg.PrefixEnv("ASSUMER_"), func() (*sts.Client, error) {
-		cfg, err := config.LoadDefaultConfig(context.Background())
+		cfg, err := config.LoadDefaultConfig(ctx)
 		if err != nil {
 			return nil, err
 		}
@@ -116,10 +85,13 @@ func main() {
 		return sts.NewFromConfig(cfg), nil
 	}))
 
+	awsApi := must2(aws.NewAwsApi(ctx, stsClient))
 	// check which user it is
-	stsResp := must2(stsClient.GetCallerIdentity(context.Background(), &sts.GetCallerIdentityInput{}))
-	log.Printf("using assumer [%s]\n", aws.PrincipalFromSts(*stsResp.Arn))
+	_, arn := must3(awsApi.WhoAmI(ctx))
 
+	log.Printf("using assumer [%s]\n", arn)
+
+	// Authentication
 	token := auth.LoginToken{
 		Key: []byte(appCfg.SignKey),
 	}
@@ -129,53 +101,32 @@ func main() {
 		ClientId:     appCfg.GithubClientId,
 	}
 
-	e := echo.New()
-	e.Renderer = &templates.EchoRenderer{}
-
-	// Middleware
-	e.Pre(middleware.AddTrailingSlash())
-	if appCfg.DevelopmentMode {
-		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-			Format:           "${time_custom} method=${method}, uri=${uri}, status=${status}\n",
-			CustomTimeFormat: "2006/01/02 15:04:05",
-		}))
-	} else {
-		e.Use(middleware.Logger())
-	}
-	e.Use(middleware.Recover())
-	e.Use(middleware.RequestID())
-
-	e.HTTPErrorHandler = func(err error, c echo.Context) {
-		switch v := err.(type) {
-		case *echo.HTTPError:
-			c.JSON(http.StatusInternalServerError, map[string]string{
-				"error": fmt.Sprint(v.Message),
-			})
-		default:
-			c.JSON(http.StatusInternalServerError, map[string]string{
-				"error": v.Error(),
-			})
-		}
-	}
-
-	webui.Router(e, authMethod, &storage.StorageService{Storage: storageCtx.store}, token, stsClient)
-
-	log.Printf("listening [http://%s]\n", appCfg.ListenAddr)
-
 	if appCfg.GenerateRootToken {
-		accessToken, err := token.SignToken(auth.UserInfo{
+		accessToken := must2(token.SignToken(auth.UserInfo{
 			Username:  "root",
 			Email:     "root@root",
 			Superuser: true,
-		}, auth.DefaultTokenExpiration)
-		if err != nil {
-			e.StdLogger.Fatalf("failed to generate root token [%s]\n", err)
-		}
-		e.StdLogger.Printf("ROOT access token: %s\n", accessToken)
-		e.StdLogger.Printf("login with http://%s/login?token=%s\n", appCfg.ListenAddr, accessToken)
+		}, auth.DefaultTokenExpiration))
+		logger.Info("ROOT access token: %s\n", accessToken)
+		logger.Info("login with http://%s/login?token=%s\n", appCfg.ListenAddr, accessToken)
 	}
 
-	if err := e.Start(appCfg.ListenAddr); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		slog.Error("failed to start server", "error", err)
-	}
+	// Http server
+	router := http.NewServeMux()
+	router.Handle("/api", nil)
+	router.Handle("/", nil)
+	handler := addTrailingSlash(router)
+
+	log.Printf("listening [http://%s]\n", appCfg.ListenAddr)
+	must(http.ListenAndServe(appCfg.ListenAddr, handler))
+
+}
+
+func addTrailingSlash(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.HasSuffix(r.URL.Path, "/") {
+			r.URL = r.URL.JoinPath("/")
+		}
+		h.ServeHTTP(w, r)
+	})
 }
